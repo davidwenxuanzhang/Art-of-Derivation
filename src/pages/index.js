6543755e9afa897563c19777c67dc9e085292575
David Zhang <davidwenxuanzhang@gmail.com>
2026-01-20 00:50:53 -0500

    modified to About

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,7 +38,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/Introduction">
+            to="/docs/About">
             What is this?
           </Link>
         </div>
